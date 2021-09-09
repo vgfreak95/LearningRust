2e222599f6d4805034b0a8aa73bf124780ee2955
richardschroeders <rschroeders95@gmail.com>
2021-09-09 18:44:50 -0400

    learning rust

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,29 @@
+#![allow(warnings)]
+
 use std::{io, str};
 use rand::Rng;
 use std::cmp::Ordering;
 mod hangman;
 
-
-
-struct Player { // Example Structure of a player
-    is_human: bool,
-    health: i32,
-    attack: i16
-}
-
-enum CardinalDirections {
-    North,
-    South,
-    East,
-    West
+struct Book {
+    title: String,
+    author: String,
+    year_released: String
 }
 
 
 fn main() {
 
+    let Book1 = Book {
+        title: String::from("The Hunger Games"),
+        author: String::from("kate spade"),
+        year_released: String::from("1298")
+    };
+
+    let some_str = String::from("Hello");
+    let reference = &some_str;
     
-    //println!("{}", count_digits(123));
-    println!("Hello World");
-
     
 
-}
-
-fn count_digits(mut n: i32) -> i32 {
-    let mut digit_count = 0;
-    while n != 0 {
-        n = n / 10;
-        digit_count+=1;
-    }
-    digit_count
-
+    
 }
